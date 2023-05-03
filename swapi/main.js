145dc45e83fb74a88689b02fd223027ca452a2e1
Garrett Pace <elderpace6@gmail.com>
2023-05-03 17:25:27 -0600

    updates

--- swapi/main.js
+++ swapi/main.js
@@ -1,22 +1,59 @@
-let residents = document.querySelector('button')
+const maButton = document.getElementById('get-people-button')
+const maDiv = document.getElementById('people')
 
-function testButton() {
-    console.log("Millions of voices silenced")
-}
-
-residents.addEventListener('click', getResidents)
-
-
-const getResidents = () => {
-    axios.get("https://swapi.dev/api/planets/?search=alderaan")
+function fetchThosePeople(){
+    axios.get('https://swapi.dev/api/planets/?search=alderaan')
     .then((res) => {
-        for (resident in residents){
-            axios.get(resident.value)
+        let residentsUrlArray = res.data.results[0].residents
+
+        for (let i = 0; i < residentsUrlArray.length; i++) {
+            let residentUrl = residentsUrlArray[i]
+            axios.get(residentUrl)
+            .then((res2) => {
+                let newPersonPara = document.createElement('p')
+                newPersonPara.innerHTML = res2.data.name
+                maDiv.appendChild(newPersonPara)
+            })
+            .catch(() => {
+
+            })
         }
+
     })
-    .catch((err) => {
-        console.log(err)
+    .catch(() => {
+
+
     })
 }
 
+maButton.addEventListener('click', fetchThosePeople)
 
+
+// let residents = document.querySelector('button')
+
+// function testButton() {
+//     console.log("Millions of voices silenced")
+// }
+
+
+
+// const getResidents = () => {
+//     axios.get("https://swapi.dev/api/planets/?search=alderaan")
+//     .then((res) => {
+//         console.log(res.data.results[0].residents)
+//         for (resident in res.data.results[0].residents){
+//             axios.get(resident.value)
+//             .then(res2 => {
+//                 const para = document.createElement('h2')
+//                 const node = document.createTextNode(resident.value.name)
+//                 body.appendChild(para.appendChild(node))
+//             })
+//         }
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+// }
+
+
+// residents.addEventListener('click', getResidents)
