d8634da17830ed8ae4174e83534a80cfc25897ce
Garrett Pace <elderpace6@gmail.com>
2023-05-03 17:36:47 -0600

    better?

--- swapi/main.js
+++ swapi/main.js
@@ -1,8 +1,8 @@
 const maButton = document.getElementById('get-people-button')
 const maDiv = document.getElementById('people')
 
-function fetchThosePeople(){
-    axios.get('https://swapi.dev/api/planets/?search=alderaan')
+function fetchThosePeople() {
+    axios.get('http://swapi.dev/api/planets/?search=alderaan')
     .then((res) => {
         let residentsUrlArray = res.data.results[0].residents
 
@@ -18,42 +18,10 @@ function fetchThosePeople(){
 
             })
         }
-
     })
     .catch(() => {
-
 
     })
 }
 
 maButton.addEventListener('click', fetchThosePeople)
-
-
-// let residents = document.querySelector('button')
-
-// function testButton() {
-//     console.log("Millions of voices silenced")
-// }
-
-
-
-// const getResidents = () => {
-//     axios.get("https://swapi.dev/api/planets/?search=alderaan")
-//     .then((res) => {
-//         console.log(res.data.results[0].residents)
-//         for (resident in res.data.results[0].residents){
-//             axios.get(resident.value)
-//             .then(res2 => {
-//                 const para = document.createElement('h2')
-//                 const node = document.createTextNode(resident.value.name)
-//                 body.appendChild(para.appendChild(node))
-//             })
-//         }
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     })
-// }
-
-
-// residents.addEventListener('click', getResidents)
